37c33ec5e4c4c861ba47926a0e72c508188eb02a
Briih <marin.7107@outlook.com>
2018-06-19 20:02:24 -0500

    nuevo

--- js/mijs.js
+++ js/mijs.js
@@ -1,4 +1,18 @@
-function cambiarimagen() {
-    var pokeballImg = document.getElementById("icon01")
-    pokeballImg.src = "img/Totodile.png";
+function click() {
+    var ok = document.getElementById("ok");
+    ok.onclick = cambiarimagen;
 }
+
+function cambiarimagen() {
+    var pokeballImg, i = 1;
+    if (i == 1) {
+        pokeballImg = document.getElementById("icon01");
+        i = 2;
+    } else {
+        pokeballImg.src = "img/Totodile.png";
+        i = 1;
+    }
+
+}
+
+window.onload = click;
